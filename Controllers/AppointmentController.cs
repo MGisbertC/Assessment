3370c45a1caf4d93699d33e599b2644b1f71889b
Miguel Angel Gisbert Castillo <mickygisbert@gmail.com>
2025-03-11 21:39:29 -0400

    Add Manager endpoints

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -1,4 +1,5 @@
-﻿using MGisbert.Appointments.Models;
+﻿using MGisbert.Appointments.Data.Enums;
+using MGisbert.Appointments.Models;
 using MGisbert.Appointments.Models.Request;
 using MGisbert.Appointments.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -116,5 +117,72 @@ namespace MGisbert.Appointments.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
+
+        #region Manager Endpoints
+        [HttpPost("{id}/approve")]
+        public async Task<IActionResult> ApproveAppointment(int id)
+        {
+            try
+            {
+                var updatedAppointment = await _appointmentService.UpdateAppointmentStatusAsync(id, Status.Approved);
+                return Ok(updatedAppointment);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost("{id}/cancel")]
+        public async Task<IActionResult> CancelAppointment(int id)
+        {
+            try
+            {
+                var updatedAppointment = await _appointmentService.UpdateAppointmentStatusAsync(id, Status.Cancelled);
+                return Ok(updatedAppointment);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppointment(int id)
+        {
+            try
+            {
+                await _appointmentService.DeleteAppointmentAsync(id);
+                return NoContent();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllAppointments([FromQuery] string sortBy = "Date", [FromQuery] bool ascending = true)
+        {
+            try
+            {
+                var appointments = await _appointmentService.GetAllAppointmentsAsync(sortBy, ascending);
+                return Ok(appointments);
+            }
+            catch (ApplicationException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        #endregion
     }
 }
